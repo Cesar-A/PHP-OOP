8464577aa9f5e1b589a3999ba104767e4def10bb
Cesar Arroyo <cesararroyo1234@gmail.com>
2015-01-07 12:26:36 -0800

    lecture 1 re sended

--- index.php
+++ index.php
@@ -1,2 +1,33 @@
 <?php
-hi
+                //lecture 1
+//example 1
+class Car {
+    //class body
+    //class body
+    //class body
+}
+
+class Cat {
+    //class body
+    //class body
+    //class body
+}
+
+class Animal {
+    //class body
+    //class body
+    //class body
+}
+
+                            //example2
+$dog1 = newdog();
+$dog2 = newdog();
+$dog3 = newdog();
+
+                            //example3
+class Cars{
+public $name = "mustang";
+public $manufacture = "ford";
+public $color = "white";
+public $year = "2012";
+}
