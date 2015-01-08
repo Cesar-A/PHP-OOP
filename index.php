ff2e706661027267355f2cf05110c27ee2eb9928
Cesar Arroyo <cesararroyo1234@gmail.com>
2015-01-08 12:04:26 -0800

    lecture 2

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
 //lecture 1
 //example 1
 class Car {
-    //class body
-    //class body
-    //class body
+//class body
+//class body
+//class body
 }
 
 class Cat {
-    //class body
-    //class body
-    //class body
+//class body
+//class body
+//class body
 }
 
 class Animal {
-    //class body
-    //class body
-    //class body
+//class body
+//class body
+//class body
 }
 
 //example2
@@ -28,35 +28,72 @@ $dog3 = newdog();
 //example3
 class Cars {
 
-    public $name = "civic";
-    public $manufacture = "Honda";
-    public $color = "red";
-    public $year = "1991";
-    public $model = "LX";
+public $name = "civic";
+public $manufacture = "Honda";
+public $color = "red";
+public $year = "1991";
+public $model = "LX";
 
 }
 
 //lecture2\
 class soccer {
-    public $team;
-    public $league ;
-    function __construct($team , $league) 
+public $team;
+public $league;
+function __construct($team, $league)
 };
 {
-   
-        $this>
-    }
-    TeamName = $team ;
-    $this>
-    League = $league;
-   
+
+$this>
+}
+TeamName = $team;
+$this>
+League = $league;
+
 }
 
 function getInfo() {
-    return "{$this>
-    TeamName}" .
-    "{$this>
-   League
+return "{$this>
+TeamName}" .
+"{$this>
+League
 }";
 
+class human {
+public $firstName;
+public $lastName;
+
+function __construct($title, $firstName, $lastName, ) {
+$this>
+firstName = $firstName;
+$this>
+lastName = $lastName;
+
+function getName() {
+return “{$this>
+firstName}” .
+“{$this>
+lastName}”;
+}
+}
+
+class TvShow {
+public $showName;
+public $Network;
+public $channel;
+function __construct($title, $showName, $Network, $channel) {
+$this>
+show = $showName;
+$this>
+network = $Network;
+$this>
+channel = $channel;
+}
+function getName() {
+return "{$this>
+show}" .
+"{$this>
+network}";
+}
+}
 
