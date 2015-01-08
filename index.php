43fca0605a525da7be5fec422acd3521d61864b1
Cesar Arroyo <cesararroyo1234@gmail.com>
2015-01-08 11:55:52 -0800

    Lecture 2

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-                //lecture 1
+
+//lecture 1
 //example 1
 class Car {
     //class body
@@ -19,15 +20,43 @@ class Animal {
     //class body
 }
 
-                            //example2
+//example2
 $dog1 = newdog();
 $dog2 = newdog();
 $dog3 = newdog();
 
-                            //example3
-class Cars{
-public $name = "mustang";
-public $manufacture = "ford";
-public $color = "white";
-public $year = "2012";
+//example3
+class Cars {
+
+    public $name = "civic";
+    public $manufacture = "Honda";
+    public $color = "red";
+    public $year = "1991";
+    public $model = "LX";
+
 }
+
+//lecture2\
+class soccer {
+    public $team;
+    public $league ;
+    function __construct($team , $league) 
+};
+{
+   
+        $this>
+    }
+    TeamName = $team ;
+    $this>
+    League = $league;
+   
+}
+
+function getInfo() {
+    return "{$this>
+    TeamName}" .
+    "{$this>
+   League
+}";
+
+
