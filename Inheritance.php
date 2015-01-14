4903352849fc070c141563a0e858735f902e57f5
Cesar Arroyo <cesararroyo1234@gmail.com>
2015-01-13 18:03:16 -0800

    lecture number 4

---
+++ Inheritance.php
@@ -0,0 +1,22 @@
+<?php
+
+class Cars{
+    public $Honda;
+    public $Nissan;
+    public $Lexus;
+    public $Mitsubishi;
+    public $Subaru;
+    //creats classes
+    
+function __construct($Honda, $Nissan, $Lexus, $Mitsubishi, $Subaru){
+    $this->Honda=$Honda;
+    $this->Nissan=$Nissan;
+    $this->Lexus=$Lexus;
+    $this->Mitsubishi=$Mitsubishi;
+    $this->Subaru=$Subaru;
+    //calls classes and gives them a value
+        
+        
+    }
+
+}
