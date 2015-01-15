c99c6afded9dabf2e1bc78cd22de9a0d12897ccf
Cesar Arroyo <cesararroyo1234@gmail.com>
2015-01-15 12:24:03 -0800

    fixing errors

--- Inheritance.php
+++ Inheritance.php
@@ -1,27 +1,26 @@
 <?php
 
-class Cars{
+class Cars {
+
     public $Honda;
     public $Nissan;
     public $Lexus;
     public $Mitsubishi;
     public $Subaru;
+
     //creats classes
-    
-function __construct($Honda, $Nissan, $Lexus, $Mitsubishi, $Subaru){
-    $this->Honda=$Honda;
-    $this->Nissan=$Nissan;
-    $this->Lexus=$Lexus;
-    $this->Mitsubishi=$Mitsubishi;
-    $this->Subaru=$Subaru;
-    //calls classes and gives them a value
-        
-        
+
+    function __construct($Honda, $Nissan, $Lexus, $Mitsubishi, $Subaru) {
+        $this->Honda = $Honda;
+        $this->Nissan = $Nissan;
+        $this->Lexus = $Lexus;
+        $this->Mitsubishi = $Mitsubishi;
+        $this->Subaru = $Subaru;
+        //calls classes and gives them a value
     }
-    function getName(){
-        return "this car is a " .$this-> Nissan ;
-        return "this car is a " .$this-> Honda  ;
-    
+
+    function getName() {
+        return "this car is a " . $this->Nissan;
     }
     
-}
+  }
